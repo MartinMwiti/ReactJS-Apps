6e66d1afa896250633e5a7de94f32b51161c642e
MartinMwiti <martinmwiti007@gmail.com>
2020-06-08 22:48:31 +0300

    bootstrap button

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ReactJS-Apps
 Built apps using ReactJS
+
+## Dependecies
+
+**1. REST API JSON-SERVER**
+```npm i ```
+
+**2. React + Bootstrap**
+	npm install --save bootstrap
+	npm install --save reactstrap react react-dom
+	
+* [Reactstrap](https://reactstrap.github.io/)
--- bootstrap-with-react/src/App.js
+++ bootstrap-with-react/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Container, Button } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Button>Button</Button>
-    </div>
+    // '<Container>' mimics bootstraps <div class="container"></div>
+    <Container>
+
+      {/* <Button color="primary">Button</Button> {' '} */}
+      <Button color="primary" disabled={true} active={true}>Button</Button> {' '}
+      
+      <Button color="secondary" active={true}>Button</Button> {' '}
+
+      {/* <Button color="success" block={true}>Button</Button> {' '} */}
+      <Button color="success" size="sm">Button</Button> {' '}
+
+      <Button color="info" size="lg">Button</Button> {' '}
+      {/* <Button color="info" outline={true}>Button</Button> {' '} */}
+
+      <Button color="info" outline={true}>Button</Button> {' '}
+      <Button color="link">Button</Button> {' '}
+
+    </Container>
   );
 }
 
