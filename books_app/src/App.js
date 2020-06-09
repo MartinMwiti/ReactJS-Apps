f00d8de54a42b5aa0a1360d0016a395779ef58bc
MartinMwiti <martinmwiti007@gmail.com>
2020-06-09 17:14:33 +0300

    dependencies update

--- books_app/src/App.js
+++ books_app/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Table, Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App container">
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Rating</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Book1</td>
+                            <td>4.0</td>
+                            <td>Delete</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
 }
 
 export default App;
