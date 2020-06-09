fca0ad705b1a24f7c2ca0a07c8ccf89967177f39
MartinMwiti <martinmwiti007@gmail.com>
2020-06-09 18:37:43 +0300

    added modal

--- books_app/src/App.js
+++ books_app/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { Table, Button } from 'reactstrap';
+import { Table, Button, Label, Input, FormGroup, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class App extends Component {
     state = {
-        books: []
+        books: [],
+        newBookData:{
+            title: "",
+            rating: ""
+        },
+        newBookModal: false
     }
+
     componentWillMount(){
         axios.get("http://localhost:3000/books")
         .then((response)=>this.setState({
@@ -13,8 +19,15 @@ class App extends Component {
         })
         )
     };
+    // change state
+    toggleNewBookModal(){
+        this.setState({
+            newBookModal: !this.state.newBookModal
+        })
+    }
 
     render() {
+        
         let books = this.state.books.map((book)=>{
             return(
                 <tr>
@@ -33,6 +46,24 @@ class App extends Component {
         return (
             <div className="App container">
                 <Table>
+                    <Button color="primary" onClick={this.toggleNewBookModal.bind(this)}>Add Book</Button>
+                    <Modal isOpen={this.state.newBookModal}>
+                        <ModalHeader toggle={this.toggleNewBookModal.bind(this)}>Add a new book</ModalHeader>
+                        <ModalBody>
+                            <FormGroup>
+                                <Label for="title">Title</Label>
+                                <Input id="title" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="title">Rating</Label>
+                                <Input id="title" />
+                            </FormGroup>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="primary" onClick={this.toggleNewBookModal.bind(this)}>Add Book</Button>{' '}
+                            <Button color="secondary" onClick={this.toggleNewBookModal.bind(this)}>Cancel</Button>
+                        </ModalFooter>
+                    </Modal>
                     <thead>
                         <tr>
                             <th>#</th>
