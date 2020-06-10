05f269acb449d5087174d91609806eea152248bc
MartinMwiti <martinmwiti007@gmail.com>
2020-06-10 22:58:15 +0300

    search request to via REST API

--- news_app/src/components/Books.js
+++ news_app/src/components/Books.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import request from 'superagent'
 
 // components
 import SearchArea from './SearchArea'
@@ -20,12 +21,29 @@ class Books extends Component {
         })
     }
 
+    // "Get" method using entered value from searchField
+    searchBook = (e)=>{
+        e.preventDefault() // whenever we make a form submit
+        request
+          .get("https://www.googleapis.com/books/v1/volumes")
+          .query({ q: this.state.searchField })
+          .then((data) => {
+            console.log(data);
+          });
+
+    }
+
     render() {
         return (
-            <div>
-                <SearchArea handleSearch={this.handleSearch}/>
-            </div>
-        )
+          <div>
+            <SearchArea
+              searchField={this.state.searchField}
+
+              handleSearch={this.handleSearch}
+              searchBook={this.searchBook}
+            />
+          </div>
+        );
     }
 }
 
--- news_app/src/components/SearchArea.js
+++ news_app/src/components/SearchArea.js
@@ -4,21 +4,22 @@ import React from 'react'
 function SearchArea(props) {
 
     return (
-        <div className="search-area">
-            <form action="">
-                <input 
-                    className="search-box" 
-                    onChange={props.handleSearch}
-                    name="" 
-                    value="" 
-                    type="text"
-                    placeholder="Enter search"
-                />
-                <button className="search-btn" type="submit"><i className="fas fa-search" /></button>
-            </form>
-            
-        </div>
-    )
+      <div className="search-area">
+        <form action="" onSubmit={props.searchBook}>
+          <input
+            className="search-box"
+            onChange={props.handleSearch}
+            name=""
+            value={props.searchField}
+            type="text"
+            placeholder="Enter search"
+          />
+          <button className="search-btn" type="submit">
+            <i className="fas fa-search" />
+          </button>
+        </form>
+      </div>
+    );
 }
 
 export default SearchArea;
