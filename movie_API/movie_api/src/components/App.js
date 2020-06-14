99d3c8aeebc7703fde992d06e857ef7939347b67
MartinMwiti <martinmwiti007@gmail.com>
2020-06-14 21:00:56 +0300

    card update

--- movie_API/movie_api/src/components/App.js
+++ movie_API/movie_api/src/components/App.js
@@ -5,6 +5,7 @@ import '../App.css';
 import Nav from './Nav'
 import SearchArea from './SearchArea'
 import MovieList from './MovieList'
+import Pagination from './Pagination'
 
 class App extends Component {
   constructor(){
@@ -12,28 +13,50 @@ class App extends Component {
       this.state= {
         movies: [],
         searchTerm: '',
+        totalResults: 0,
+        currentPage: 1,
       }   
     // API Credentials
     this.apiKey = process.env.REACT_APP_API 
   };
 
 
+  _refreshMovies() {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}`)
+      .then((data) => data.json()) // This will return a few fields, the one you want to look at is the results field. (As stated by the api provider-MovieDB)
+      .then(data => {
+          console.log(data)
+        this.setState({ movies: [...data.results], totalResults: data.total_results })
+      })
+  }
+
+
   handleSubmit = (e) => {
     e.preventDefault();
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}`)
-    .then((data) => data.json()) // This will return a few fields, the one you want to look at is the results field. (As stated by the api provider-MovieDB)
-    .then(data => {
-        console.log(data)
-      this.setState({movies: [...data.results]})
-    })
+    this._refreshMovies()
+    
   };
 
   handleChange = (e)=>{ // fills out the searchTerm
     this.setState({searchTerm: e.target.value})
   }
 
+
+  // PAGINATION
+  nextPage = (pageNumber)=>{
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.pageNumber}`)
+      .then((data) => data.json()) // This will return a few fields, the one you want to look at is the results field. (As stated by the api provider-MovieDB)
+      .then(data => {
+          console.log(data)
+        this.setState({ movies: [...data.results], currentPage: pageNumber})
+      })
+  }
+
   
   render() {
+
+    const numberPages = Math.floor(this.state.totalResults / 20)
+
     return (
       <div>
         <Nav />
@@ -45,6 +68,13 @@ class App extends Component {
         <MovieList 
           movies={this.state.movies}
         />
+        { 
+          this.state.totalResults > 20 
+          ? 
+          <Pagination pages={numberPages} nextPage={this.nextPage} currentPage={this.state.currentPage} />
+          :
+          '' // display nothing if there in no pagination       
+        }
       </div>
     )
   };
---
+++ movie_API/movie_api/src/components/Pagination.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Pagination = (props) =>{
+    const pageLinks = []
+
+    for()
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Pagination;
