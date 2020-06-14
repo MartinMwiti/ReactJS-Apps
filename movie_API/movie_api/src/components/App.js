ff9f5b88baf8e971a19309a16a8340c9923cc79d
MartinMwiti <martinmwiti007@gmail.com>
2020-06-14 14:07:36 +0300

    navbar+searchArea added

---
+++ movie_API/movie_api/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import '../App.css';
+
+// Components
+import Nav from './Nav'
+import SearchArea from './SearchArea'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <SearchArea />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ movie_API/movie_api/src/components/SearchArea.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const SearchArea = () =>{
+    return (
+        <div className="container">
+
+            <div className="row">
+                <section className="col s4 offset-s4">
+                    <form action="">
+
+                        <div className="input-field">
+                            <input type="text" name="" value="" placeholder="Search movies..." />
+                        </div>
+                        
+                    </form>
+                </section>
+            </div>
+            
+        </div>
+    )
+}
+
+export default SearchArea;
