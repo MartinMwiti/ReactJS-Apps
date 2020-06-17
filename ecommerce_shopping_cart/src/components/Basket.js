e3a132a408dbf249c0cdd1a5c0a2fb15669e80d7
MartinMwiti <martinmwiti007@gmail.com>
2020-06-17 20:07:46 +0300

    remove items from cart update

--- ecommerce_shopping_cart/src/components/Basket.js
+++ ecommerce_shopping_cart/src/components/Basket.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import util from "../util";
+
 class Basket extends Component {
     render() {
 
@@ -19,18 +21,36 @@ class Basket extends Component {
                 <ul className="text-left">
                   {cartItems.map((item) => (
                     <li key={item.id}>
-                      <b>{item.title}</b> X {item.count}
+                      <b>{item.title}</b>
                       <button
-                        className="btn btn-danger ml-2"
+                        className="btn btn-danger btn-xs float-right"
                         onClick={(e) =>
-                          this.props.handleRemoveFromCart(e, item)
+                          this.props.handleRemoveFromCart(
+                            this.props.cartItems,
+                            item
+                          )
                         }
                       >
                         X
                       </button>
+                      <br />
+                      {item.count} X {util.formatCurrency(item.price)}
                     </li>
                   ))}
                 </ul>
+
+                <b className="mr-3">
+                  Sum:{" "}
+                  {util.formatCurrency(
+                    cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                  )}
+                </b>
+                <button
+                  onClick={() => alert("Todo: Implement checkout page.")}
+                  className="btn btn-primary"
+                >
+                  checkout
+                </button>
               </div>
             )}
           </div>
